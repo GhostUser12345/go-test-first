fafb33fb2f595b4bf48ae93ff6a53ae9eec2277e
Alexander Nikolaev <nikolaevsailing@mail.ru>
2022-04-01 10:14:55 +0300

    strings

--- first.go
+++ first.go
@@ -5,5 +5,9 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, Git!")
+	var a = "text \n text"
+	var b = `text \n text`
+	var c rune = 960
+	var d = '*'
+	fmt.Printf("%v\n%v\n%c\n%[4]T\n%c", a, b, c, d, b[1])
 }
