a9cdb8c3d232570347144a6fa05d80f1ebe3a86b
Alexander Nikolaev <nikolaevsailing@mail.ru>
2022-04-03 11:51:05 +0300

    rot13 decoder

--- first.go
+++ first.go
@@ -9,5 +9,17 @@ func main() {
 	var b = `text \n text`
 	var c rune = 960
 	var d = '*'
-	fmt.Printf("%v\n%v\n%c\n%[4]T\n%c", a, b, c, d, b[1])
+	fmt.Printf("%v\n%v\n%c\n%[4]T\n%c\n", a, b, c, d, b[1])
+	const mess = "uv vagreangvbany fcnpr fgngvba"
+	rot13Decoder(mess)
+}
+
+func rot13Decoder(mess string) {
+	for i := 0; i < len(mess); i++ {
+		var letter = mess[i] + 13
+		if letter > 'z' {
+			letter = letter - 26
+		}
+		fmt.Printf("%c", letter)
+	}
 }
